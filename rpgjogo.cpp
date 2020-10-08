916331c9a474a5e3aa920e41b11afd6827c27059
Alucard32x <69759759+Alucard32x@users.noreply.github.com>
2020-10-08 19:20:57 -0300

    Add files via upload

---
+++ rpgjogo.cpp
@@ -0,0 +1,17 @@
+//Código do jogo
+
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+#include<stdio.h>
+#include<fstream>
+#include<locale.h>
+
+using namespace std;
+
+int main(){
+setlocale(LC_ALL,"portuguese");
+cout<<("Bem vindo a casa de Svérkèr!!! Espero que possamos estar em concordância e harmonia!!!");
+system("pause");
+return 0;
+}
